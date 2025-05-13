1f532376818e27386f401a65247c90416c9853de
hamoudilaz <hamoudi_lazkani@hotmail.se>
2025-05-13 11:45:16 +0200

    Merging with fizzah

--- Twitter-Clone-Frontend/src/services/register.js
+++ Twitter-Clone-Frontend/src/services/register.js
@@ -1,66 +1,40 @@
 export const registerUser = async (params) => {
-    try {
-        const response = await fetch(`http://localhost:3000/register`, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            credentials: 'include',
-            body: JSON.stringify(params),
-        });
-        const data = await response.json();
-        console.log(data)
-        if (data.error) {
-            console.error("Error registering user:", data.error)
-            return { error: data.error };
-        }
-        return { message: data.message };
-
-    } catch (err) {
-        return err
+  try {
+    const response = await fetch(`http://localhost:3000/register`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      credentials: "include",
+      body: JSON.stringify(params),
+    });
+    const data = await response.json();
+    console.log(data);
+    if (data.error) {
+      console.error("Error registering user:", data.error);
+      return { error: data.error };
     }
-}
+    return { message: data.message };
+  } catch (err) {
+    return err;
+  }
+};
 
 export const loginUser = async (params) => {
-    try {
-        const response = await fetch(`http://localhost:3000/login`, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            credentials: 'include',
-            body: JSON.stringify(params),
-        });
+  try {
+    const response = await fetch(`http://localhost:3000/login`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      credentials: "include",
+      body: JSON.stringify(params),
+    });
 
-        const data = await response.json();
-        console.log(data)
-        if (data.error) {
-            console.error("Error registering user:", data.error)
-            return { error: data.error };
-        }
-        return { message: data.message };
-
-    } catch (err) {
-        return err
+    const data = await response.json();
+    console.log(data);
+    if (data.error) {
+      console.error("Error registering user:", data.error);
+      return { error: data.error };
     }
-}
-
-
-
-export const postTweet = async (tweet) => {
-    try {
-        const response = await fetch(`http://localhost:3000/tweet`, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            credentials: 'include',
-            body: JSON.stringify(tweet),
-        });
-
-        const data = await response.json();
-        console.log(data)
-        if (data.error) {
-            console.error("Error Posting tweet:", data.error)
-            return { error: data.error };
-        }
-        return { message: data.message };
-
-    } catch (err) {
-        return err
-    }
-}
+    return { message: data.message };
+  } catch (err) {
+    return err;
+  }
+};
