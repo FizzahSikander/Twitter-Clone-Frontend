3fb0701182e17d232e40907a7014a338acefca35
Fizzah Sikander <123269709+FizzahSikander@users.noreply.github.com>
2025-05-13 11:40:22 +0200

    tweet fetch

--- Twitter-Clone-Frontend/src/services/register.js
+++ Twitter-Clone-Frontend/src/services/register.js
@@ -43,24 +43,3 @@ export const loginUser = async (params) => {
 
 
 
-export const postTweet = async (tweet) => {
-    try {
-        const response = await fetch(`http://localhost:3000/tweet`, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            credentials: 'include',
-            body: JSON.stringify(tweet),
-        });
-
-        const data = await response.json();
-        console.log(data)
-        if (data.error) {
-            console.error("Error Posting tweet:", data.error)
-            return { error: data.error };
-        }
-        return { message: data.message };
-
-    } catch (err) {
-        return err
-    }
-}
