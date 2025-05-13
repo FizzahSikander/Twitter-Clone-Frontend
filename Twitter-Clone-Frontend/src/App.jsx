fe6fb07fe696a995fc7e35b8bb625ca327303105
hamoudilaz <hamoudi_lazkani@hotmail.se>
2025-05-13 11:17:51 +0200

    merge log&reg

--- Twitter-Clone-Frontend/src/App.jsx
+++ Twitter-Clone-Frontend/src/App.jsx
@@ -1,20 +1,19 @@
-import { useState } from 'react';
-import { Login } from './pages/Login';
-import { Routes, Route, Navigate } from 'react-router-dom';
-import { Register } from './pages/Register';
-import HomePage from './pages/mockup';
-
+import { useState } from "react";
+import { Login } from "./pages/Login";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { Register } from "./pages/Register";
+// import { Home } from "./pages/home";
 function App() {
   // const [count, setCount] = useState(0);
 
   return (
     <>
       <Routes>
-        <Route path='/' element={<Navigate to='/login' replace />} />
+        <Route path="/" element={<Navigate to="/login" replace />} />
 
-        <Route path='/login' element={<Login />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='/home' element={<HomePage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        {/* <Route path="/home" element={<Home />} /> */}
 
         {/* <Route path="/feed" element={<Feed />} />
       <Route path="/profile" element={<Profile />} /> */}
