21d03f1107c04719493e06bec613fd226cca9186
hamoudilaz <hamoudi_lazkani@hotmail.se>
2025-05-15 09:18:22 +0200

    Save local changes

--- Twitter-Clone-Frontend/src/App.jsx
+++ Twitter-Clone-Frontend/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
 
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route path="/profile/:id" element={<Profile />} />
 
         {/* <Route path="/feed" element={<Feed />} />
       <Route path="/profile" element={<Profile />} /> */}
