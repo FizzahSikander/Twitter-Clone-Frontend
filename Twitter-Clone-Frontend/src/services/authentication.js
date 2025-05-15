1af787bdb83c13c84f4d44ec6fae596737ee167c
Khandaker Toihidul Islam <it.toihid@gmail.com>
2025-05-15 09:45:51 +0200

    home mockup and inprogress of saving tweet

---
+++ Twitter-Clone-Frontend/src/services/authentication.js
@@ -0,0 +1,18 @@
+export const Auth = async () => {
+  try {
+    const response = await fetch(`http://localhost:3000/validate`, {
+      method: "GET",
+      credentials: "include",
+    });
+    const data = await response.json();
+
+    if (data.error) {
+      console.error("user is not valid:", data.error);
+      return { error: data.error };
+    }
+    //setUserId(data.user.id);
+    return data.user.id;
+  } catch (err) {
+    return err;
+  }
+};
