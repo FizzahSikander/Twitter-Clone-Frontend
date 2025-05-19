7d9c9b7d3fd13db6304a756d47ad109f771ef0d9
Hamoudi Lazkani <hamoudi_lazkani@hotmail.se>
2025-05-19 16:52:20 +0200

    Feature: Follow/Unfollow

---
+++ Twitter-Clone-Frontend/src/services/actions.js
@@ -0,0 +1,38 @@
+export const followUser = async (target, mode) => {
+    try {
+        const res = await fetch(`http://localhost:3000/users/${target}/${mode}/`, {
+            method: 'POST',
+            credentials: 'include',
+        });
+
+        const data = await res.json();
+
+        if (!data.ok) {
+            console.error(data.error);
+            return data.error;
+        }
+        return data.message;
+    } catch (err) {
+        return err;
+    }
+};
+
+
+
+
+
+export const loadProfile = async (username) => {
+    try {
+        const res = await fetch(`http://localhost:3000/profile/${username}`);
+        const data = await res.json();
+
+        if (data.error) {
+            console.error(data.error);
+            return data.error;
+        }
+
+        return data.user
+    } catch (err) {
+        return err
+    }
+};
