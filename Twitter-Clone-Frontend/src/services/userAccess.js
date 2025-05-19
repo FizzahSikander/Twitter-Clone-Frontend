a75774dff3e4e682d4dbdc508cad2286d70076ae
Hamoudi Lazkani <hamoudi_lazkani@hotmail.se>
2025-05-19 09:54:36 +0200

    Overwrite

--- Twitter-Clone-Frontend/src/services/userAccess.js
+++ Twitter-Clone-Frontend/src/services/userAccess.js
@@ -46,26 +46,3 @@ export const loginUser = async (params) => {
     return err;
   }
 };
-
-
-
-// export const postTweet = async (tweet) => {
-//   try {
-//     const response = await fetch(`http://localhost:3000/tweet`, {
-//       method: "POST",
-//       headers: { "Content-Type": "application/json" },
-//       credentials: "include",
-//       body: JSON.stringify({ tweet }),
-//     });
-
-//     const data = await response.json();
-//     console.log(data);
-//     if (data.error) {
-//       console.error("Error registering user:", data.error);
-//       return { error: data.error };
-//     }
-//     return { message: data.message };
-//   } catch (err) {
-//     return err;
-//   }
-// };
