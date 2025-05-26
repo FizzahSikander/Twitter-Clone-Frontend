2ca5f377529a94f1d5315454e6d67bffab36edd7
hamoudilaz <hamoudi_lazkani@hotmail.se>
2025-05-26 11:19:20 +0200

    feat: edit profile img

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -14,6 +14,8 @@ export const Profile = () => {
   const [profile, setProfile] = useState(null);
   const [loading, setLoading] = useState(true);
   const [tweets, setTweets] = useState();
+  const [img, setImg] = useState();
+  const [banner, setBanner] = useState();
 
   useEffect(() => {
     const fetchProfile = async () => {
@@ -52,15 +54,50 @@ export const Profile = () => {
 
         <div className='content'>
           <div className='banner'>
-            <img
-              src='https://placehold.co/600x200?text=Banner+Image'
-              alt='Banner'
-              className='banner-img'
+            {img ? (
+              <img src={URL.createObjectURL(img)} alt='pic' className='banner-img' />
+            ) : (
+              <label htmlFor='imageUpload'>
+                <img
+                  src='https://placehold.co/600x200?text=Banner+Image'
+                  alt='Banner'
+                  className='banner-img'
+                />
+              </label>
+            )}
+
+            <input
+              type='file'
+              accept='image/*'
+              id='imageUpload'
+              hidden
+              onChange={(e) => setImg(e.target.files[0])}
             />
           </div>
           <div className='profile-content'>
             <div className='profile-pic-container'>
-              <img src={profile.image} alt='Profile' className='profile-pic' />
+              <div className='profile-pic-wrapper'>
+                <img
+                  src={img ? URL.createObjectURL(img) : profile.image}
+                  alt='Profile'
+                  className='profile-pic'
+                />
+
+                {ownProfile && (
+                  <label htmlFor='imageUpload' className='edit-pen'>
+                    <svg
+                      xmlns='http://www.w3.org/2000/svg'
+                      height='24px'
+                      viewBox='0 0 24 24'
+                      width='24px'
+                      fill='black'
+                    >
+                      <path d='M0 0h24v24H0V0z' fill='none' />
+                      <path d='M14.06 9.02l.92.92L5.92 19H5v-.92l9.06-9.06M17.66 3c-.25 0-.51.1-.7.29l-1.83 1.83 3.75 3.75 1.83-1.83c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.2-.2-.45-.29-.71-.29zm-3.6 3.19L3 17.25V21h3.75L17.81 9.94l-3.75-3.75z' />
+                    </svg>
+                  </label>
+                )}
+              </div>
             </div>
 
             <div className='profile-info'>
