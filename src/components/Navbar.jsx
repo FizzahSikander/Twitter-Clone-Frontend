1cdbb1cb0b216b733bff04699b87c18a9e61b815
Khandaker Toihidul Islam <it.toihid@gmail.com>
2025-05-26 11:18:06 +0200

    new design

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,33 @@
-import { Link } from 'react-router-dom';
-import '../styles/navbar.css';
-import { useUser } from '../utils/UserContext';
+import { Link, useLocation } from "react-router-dom";
+import "../styles/navbar.css";
+import { useUser } from "../utils/UserContext";
 
 export default function Navbar() {
   const { user } = useUser();
+  const location = useLocation();
 
   if (!user) return null;
   return (
-    <nav className='navbar'>
-      <div className='navbar-content'>
-        <Link to='/' className='navbar-logo'>
+    <nav className="navbar">
+      <div className="navbar-content">
+        <Link to="/" className="navbar-logo">
           Twitter
         </Link>
-        <div className='navbar-links'>
-          <Link to='/home'>Home</Link>
-          <Link to={`/profile/${user.nickname}`}>Profile</Link>
+        <div className="navbar-links">
+          <Link
+            to="/home"
+            className={location.pathname === "/home" ? "active-page" : ""}
+          >
+            Home
+          </Link>
+          <Link
+            to={`/profile/${user.nickname}`}
+            className={
+              location.pathname.startsWith("/profile/") ? "active-page" : ""
+            }
+          >
+            Profile
+          </Link>
         </div>
       </div>
     </nav>
